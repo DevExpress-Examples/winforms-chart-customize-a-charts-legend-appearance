f3a51878b3ff7e04f3ebda25d2f38c95f51c08ed
DevExpressExampleBot <fakeEmail>
2022-11-10 17:46:53 +0400

    Source auto update [skip ci]

--- VB/ChartLegend/Form1.vb
+++ VB/ChartLegend/Form1.vb
@@ -2,21 +2,22 @@ Imports System
 Imports System.Drawing
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
-' ...
 
+' ...
 Namespace ChartLegend
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
+
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create an empty chart.
-            Dim chartControl1 As New ChartControl()
-
+            Dim chartControl1 As ChartControl = New ChartControl()
             ' Create a series and add points to it.
-            Dim series1 As New Series("Series 1", ViewType.Bar)
+            Dim series1 As Series = New Series("Series 1", ViewType.Bar)
             series1.Points.Add(New SeriesPoint("A", New Double() {4}))
             series1.Points.Add(New SeriesPoint("B", New Double() {2}))
             series1.Points.Add(New SeriesPoint("C", New Double() {17}))
@@ -24,69 +25,54 @@ Namespace ChartLegend
             series1.Points.Add(New SeriesPoint("E", New Double() {17}))
             series1.Points.Add(New SeriesPoint("F", New Double() {12}))
             series1.Points.Add(New SeriesPoint("G", New Double() {15}))
-
             ' Add the series to the chart.
             chartControl1.Series.Add(series1)
-
             ' Create a constant line (optional).
-            Dim constantLine1 As New ConstantLine("Constant Line 1")
+            Dim constantLine1 As ConstantLine = New ConstantLine("Constant Line 1")
             CType(chartControl1.Diagram, XYDiagram).AxisY.ConstantLines.Add(constantLine1)
             constantLine1.AxisValue = 4.5
-
             ' Create a strip (optional).
-            Dim strip1 As New Strip("Strip 1")
+            Dim strip1 As Strip = New Strip("Strip 1")
             CType(chartControl1.Diagram, XYDiagram).AxisY.Strips.Add(strip1)
             strip1.MaxLimit.AxisValue = 15
             strip1.MinLimit.AxisValue = 7.5
-
             ' Customize the strip's and constant line's legend appearance.
             strip1.ShowInLegend = True
             constantLine1.ShowInLegend = True
             constantLine1.LegendText = "Some Threshold"
-
             ' Display the chart control's legend.
             chartControl1.Legend.Visible = True
-
             ' Define its horizontal and vertical alignment.
             chartControl1.Legend.AlignmentHorizontal = LegendAlignmentHorizontal.RightOutside
             chartControl1.Legend.AlignmentVertical = LegendAlignmentVertical.Top
-
             ' Define the layout of items within the legend.
             chartControl1.Legend.Direction = LegendDirection.LeftToRight
             chartControl1.Legend.EquallySpacedItems = True
             chartControl1.Legend.HorizontalIndent = 4
             chartControl1.Legend.VerticalIndent = 4
             chartControl1.Legend.TextOffset = 4
-
             ' Define the limits for the legend to occupy the chart's space.
             chartControl1.Legend.MaxHorizontalPercentage = 50
             chartControl1.Legend.MaxVerticalPercentage = 50
-
             ' Define the legend markers' options.
             chartControl1.Legend.MarkerVisible = True
             chartControl1.Legend.MarkerSize = New Size(20, 20)
-
             ' Customize the legend appearance.
             chartControl1.Legend.BackColor = Color.LightBlue
             chartControl1.Legend.FillStyle.FillMode = FillMode.Empty
-
             chartControl1.Legend.Border.Color = Color.DarkBlue
             chartControl1.Legend.Border.Thickness = 2
             chartControl1.Legend.Border.Visible = True
-
             chartControl1.Legend.Shadow.Visible = True
             chartControl1.Legend.Shadow.Color = Color.LightGray
             chartControl1.Legend.Shadow.Size = 2
-
             ' Customize the legend text properties.
             chartControl1.Legend.Antialiasing = False
-            chartControl1.Legend.Font = New Font(Me.Font.FontFamily.Name, 9, FontStyle.Bold)
+            chartControl1.Legend.Font = New Font(Font.FontFamily.Name, 9, FontStyle.Bold)
             chartControl1.Legend.TextColor = Color.DarkBlue
-
             ' Add the chart to the form.
             chartControl1.Dock = DockStyle.Fill
             Me.Controls.Add(chartControl1)
         End Sub
-
     End Class
 End Namespace
